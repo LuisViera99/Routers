b61f26682f3a2b4e3f24451a0f315d63b0cde14d
LuisViera99 <luis.viera@cceo.com.mx>
2023-02-22 11:35:31 -0600

    iniciamos

---
+++ src/views/PokemonsView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import axios from 'axios'
+
+import {ref} from 'vue'
+
+import { RouterLink } from 'vue-router';
+
+const pokemons = ref([])
+
+const getData = async() => {
+    try{
+        const {data} = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+        //console.log(data.results);
+        pokemons.value = data.results
+
+    }catch(error){
+        console.log(error);
+    }
+};
+
+getData();
+
+</script>
+
+<template>
+    <h1>Pokemons</h1>
+    
+    <ul>
+        <li v-for="(poke, index) in pokemons" :key="index">
+           
+           <RouterLink :to="`/pokemons/${poke.name}`">{{ poke.name }}</RouterLink>
+           
+        </li>
+    </ul>
+    
+</template>
+
